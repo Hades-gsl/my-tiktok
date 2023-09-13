171332f9285002840fc2178eead1469e559e5c6e
hades <gushilei666@qq.com>
2023-09-13 14:46:26 +0800

    feat: fix script to be fit for sh

--- build.sh
+++ build.sh
@@ -1,27 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
 dir=$(pwd)
-svrs=("service/feed" "service/user" "service/publish" "service/favorite" "service/control")
+svrs="service/feed service/user service/publish service/favorite service/control"
 
-if [[ "$1" == "build" ]]; then
-    for svr in "${svrs[@]}"; do
-        cd "$dir/$svr" && sh build.sh
-        echo build $(basename "$svr") success
+if [ "$1" = "build" ]; then
+    for svr in $svrs; do
+        (cd "$dir/$svr" && sh build.sh)
+        echo "build $(basename "$svr") success"
     done
-elif [[ "$1" == "run" ]]; then
-    for svr in "${svrs[@]}"; do
-        cd "$dir/$svr" && sh build.sh
-        echo build $(basename "$svr") success
+elif [ "$1" = "run" ]; then
+    for svr in $svrs; do
+        (cd "$dir/$svr" && output/bin/$(basename "$svr")&)
+        echo "run $(basename "$svr") success"
     done
-    for svr in "${svrs[@]}"; do
-        cd "$dir/$svr" && output/bin/$(basename "$svr")&
-        echo run $(basename "$svr") success
-    done
-elif [[ "$1" == "end" ]]; then
-    for svr in "${svrs[@]}"; do
-        pid=$(ps aux | grep output/bin/$(basename "$svr") | grep -v grep | awk '{print $2}')
+elif [ "$1" = "end" ]; then
+    for svr in $svrs; do
+        pid=$(ps aux | grep "output/bin/$(basename "$svr")" | grep -v grep | awk '{print $2}')
         kill $pid
-        echo kill $(basename "$svr") success
+        echo "kill $(basename "$svr") success"
+    done
+elif [ "$1" = "clean" ]; then
+    for svr in $svrs; do
+        (cd "$dir/$svr" && rm -rf output)
+    done
+elif [ "$1" = "debug" ]; then
+    for svr in $svrs; do
+        (cd "$dir/$svr" && sh build.sh)
+        echo "build $(basename "$svr") success"
+    done
+    for svr in $svrs; do
+        (cd "$dir/$svr" && output/bin/$(basename "$svr")&)
+        echo "run $(basename "$svr") success"
     done
 fi
-
